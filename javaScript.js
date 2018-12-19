2c6f364e7d174c92a2b8d656ec3c477653e4c051
Priscilla Infante <U91607@ust-global.com>
2018-12-19 13:19:33 -0600

    style

--- javaScript.js
+++ javaScript.js
@@ -14,9 +14,7 @@ document.writeln("<h2> This is H2 </h2>");
 document.writeln("<h3 style='color:	#ff084a;'>This is H3</h3>");
 
 function paint(){
-    document.getElementById("dynamicSection");
-
-    var section = document.getElementById("dynamicSection");
+       var section = document.getElementById("dynamicSection");
 
     section.className = "highlight";
 }
